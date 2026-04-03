14fd3faa87e40fcf783d808aea76de17f52c13ea
Ajithkum179 <ajithkumarcs7639@gmail.com>
2026-04-03 08:44:47 +0530

    Update Project

---
+++ src/components/galleryData.js
@@ -0,0 +1,52 @@
+const galleryData = [
+  {
+    id: 1,
+    title: "Mountain",
+    description: "Beautiful mountain view",
+    image: "https://picsum.photos/id/1018/300/200"
+  },
+  {
+    id: 2,
+    title: "Beach",
+    description: "Sunny beach",
+    image: "https://picsum.photos/id/1015/300/200"
+  },
+  {
+    id: 3,
+    title: "Sun Forest",
+    description: "Sun Light forest",
+    image: "https://picsum.photos/id/1037/300/200"
+  },
+  {
+    id: 4,
+    title: "Newyork",
+    description: "Rich City",
+    image: "https://picsum.photos/id/1067/300/200"
+  },
+  {
+    id: 1,
+    title: "Sand",
+    description: "Beautiful view",
+    image: "https://picsum.photos/id/1055/300/200"
+  },
+  {
+    id: 2,
+    title: "Water Mountain",
+    description: "Holiday Place ",
+    image: "https://picsum.photos/id/1044/300/200"
+  },
+  {
+    id: 3,
+    title: "Forest",
+    description: "Dark forest",
+    image: "https://picsum.photos/id/1043/300/200"
+  },
+  {
+    id: 4,
+    title: "Foggey Forest",
+    description: "White Green Place",
+    image: "https://picsum.photos/id/1021/300/200"
+  }
+];
+
+export default galleryData;
